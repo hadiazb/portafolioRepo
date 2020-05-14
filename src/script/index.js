e45f7e769ac5d18aabc4d8b60430eb146841e63b
hadiazb <hadiazb@unal.edu.co>
2020-05-14 16:34:31 -0500

    Añadiendo sección de proyectos personales

---
+++ src/script/index.js
@@ -0,0 +1,37 @@
+var indice = 1;
+mostrarSlides(indice);
+
+function avanzaSlide(n) {
+    mostrarSlides(indice+=n);
+}
+
+function posicionSlide(n) {
+    mostrarSlides(indice=n);
+}
+
+
+setInterval(function time() {
+    mostrarSlides(indice+=1);
+}, 5000)
+
+function mostrarSlides(n) {
+    let slides = document.getElementsByClassName('myProyects__slider');
+    let circulos = document.getElementsByClassName('myProyects__circulos-circles');
+    
+    if (n > slides.length) {
+        indice = 1;
+    }
+    if (n < 1) {
+        indice = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none'
+    }
+    for (let i = 0; i < circulos.length; i++) {
+        circulos[i].className = circulos[i].className.replace('active', '');
+    }
+
+    slides[indice - 1].style.display = 'block';
+    circulos[indice -1].className += 'active';
+}
