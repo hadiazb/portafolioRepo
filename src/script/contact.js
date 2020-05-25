a916c9eac173bb6b1f314603540febb918f3e54d
hadiazb <hadiazb@unal.edu.co>
2020-05-24 22:35:44 -0500

    Haciendo otro nuevo push

--- src/script/contact.js
+++ src/script/contact.js
@@ -1,20 +1,17 @@
-const flag = window.matchMedia('screen and (min-width:500px)');
+const flag = window.matchMedia('screen and (max-width:500px)');
 
 flag.addListener(event);
 
 function event(event) {
   if (event.matches) {
     console.log(flag.matches);
-    document.getElementById('contact').style.display = 'none';
-    document.getElementById('sobremy').style.padding = '50px 20px 0 20px' ;
-    document.getElementById('contenedor').style.display = 'flex';
-
-  } else {
-    console.log(flag.matches);
     document.getElementById('contact').style.display = 'block'
     document.getElementById('sobremy').style.padding = '250px 20px 0 20px' ;
     document.getElementById('contenedor').style.display = 'none';
-
-
+  } else {
+    console.log(flag.matches);
+    document.getElementById('contact').style.display = 'none';
+    document.getElementById('sobremy').style.padding = '50px 20px 0 20px' ;
+    document.getElementById('contenedor').style.display = 'flex';
   }
 }
