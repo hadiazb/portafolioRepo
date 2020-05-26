face0d1edf89e97bedbb7f554bbcb0bb1e112e61
hadiazb <hadiazb@unal.edu.co>
2020-05-26 00:49:28 -0500

    Agregando tipografia y estilos

--- src/script/contact.js
+++ src/script/contact.js
@@ -1,14 +1,15 @@
 const flag = window.matchMedia('screen and (max-width:500px)');
+console.log(flag.matches);
 
 flag.addListener(event);
 
 function event(event) {
   if (event.matches) {
-    console.log(flag.matches);
+    // console.log(flag.matches);
     document.getElementById('contact').style.display = 'block'
     document.getElementById('sobremy').style.padding = '250px 20px 0 20px' ;
   } else {
-    console.log(flag.matches);
+    // console.log(flag.matches);
     document.getElementById('contact').style.display = 'none';
     document.getElementById('sobremy').style.padding = '50px 20px 0 20px' ;
   }
